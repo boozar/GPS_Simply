10b2c6eda89525aedc125d431d433fda30ca7388
boozar <boozarnovak@gmail.com>
2019-03-10 22:30:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gps_simply/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.gps_simply;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvEnabledGPS, tvStatusGPS, tvLocationGPS;
+    TextView tvEnabledNet, tvStatusNet, tvLocationNet;
+
+    ConstraintLayout homeActivity;
+
+    private LocationManager locationManager;
+    StringBuilder sbGPS = new StringBuilder();
+    StringBuilder sbNet = new StringBuilder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        homeActivity = findViewById(R.id.homeActivity);
+        homeActivity.setBackgroundColor(Color.CYAN);
+        tvEnabledGPS =  findViewById(R.id.tvEnabledGPS);
+        tvStatusGPS =  findViewById(R.id.tvStatusGPS);
+        tvLocationGPS =  findViewById(R.id.tvLocationGPS);
+        tvEnabledNet =  findViewById(R.id.tvEnabledNet);
+        tvStatusNet =  findViewById(R.id.tvStatusNet);
+        tvLocationNet = findViewById(R.id.tvLocationNet);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            checkEnabled();
+        }
+
+        //@SuppressLint("MissingPermission")
+        @Override
+        public void onProviderEnabled(String provider) {
+            checkEnabled();
+
+            if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                Toast.makeText(getBaseContext(), "GPS access restricted", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                tvStatusGPS.setText("Status: " + String.valueOf(status));
+            } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+                tvStatusNet.setText("Status: " + String.valueOf(status));
+            }
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    //@SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            Toast.makeText(getBaseContext(), "GPS access restricted", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 10, 10, locationListener);
+        checkEnabled();
+    }
+
+    private void showLocation(Location location) {
+        if (location == null) {
+            Toast.makeText(getBaseContext(), "Can't get current location", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            {
+                tvLocationGPS.setText(formatLocation(location));
+                Toast.makeText(getBaseContext(), "GPS coordinates", Toast.LENGTH_SHORT).show();
+            }
+        } else if (location.getProvider().equals(
+                LocationManager.NETWORK_PROVIDER)) {
+            tvLocationNet.setText(formatLocation(location));
+            Toast.makeText(getBaseContext(), "Network provider coordinates", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String formatLocation(Location location) {
+        if (location == null)
+            return "";
+        return String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f, time = %3$tF %3$tT",
+                location.getLatitude(), location.getLongitude(), new Date(location.getTime()));
+    }
+
+    private void checkEnabled() {
+        tvEnabledGPS.setText("Enabled: " + locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
+        tvEnabledNet.setText("Enabled: " + locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER));
+    }
+
+    public void onClickLocationSettings(View view) {
+        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+    }
+
+}
